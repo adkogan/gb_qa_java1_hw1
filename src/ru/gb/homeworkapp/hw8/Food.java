b3df9466989cbd153f22bf886e717cff87008629
Anna Kogan <adkogan@gmail.com>
2022-06-29 11:55:12 +0300

    HW8

---
+++ src/ru/gb/homeworkapp/hw8/Food.java
@@ -0,0 +1,45 @@
+package ru.gb.homeworkapp.hw8;
+
+import java.util.Random;
+
+import static ru.gb.homeworkapp.hw8.GameSnake.*;
+
+public class Food extends Cell {
+
+    private Random random;
+    private Snake snake;
+    private Poison poison;
+
+    public Food(Snake snake){
+        super(-1, -1, GameSnake.CELL_SIZE, FOOD_COLOR);
+        random = new Random();
+        this.snake = snake;
+    }
+
+    public boolean isFood(int x, int y){
+        return (getX() == x) && (getY() == y);
+    }
+
+    public boolean isEaten() {
+        return (getX() == -1);
+    }
+
+    public void eat(){
+        set(-1, -1);
+    }
+
+    public void appear(){
+        int x, y;
+        do{
+            x = random.nextInt(CANVAS_WIDTH);
+            y = random.nextInt(CANVAS_HEIGHT);
+        }while(snake.isInSnake(x, y)
+                || poison.isPoison(x, y));
+        set(x, y);
+    }
+
+    public void setPoison(Poison poison) {
+        this.poison = poison;
+    }
+
+}
